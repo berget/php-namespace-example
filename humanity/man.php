50c204dfc08dabad7aa1a73328493ed4a5f85783
dark <darkrewrite@gmail.com>
2018-06-29 01:00:40 +0800

    add new class

---
+++ humanity/man.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2018-06-28 22:41:28
+ * @version $Id$
+ */
+
+namespace rewrite\humanity;
+
+/**
+ * 
+ */
+class Man{
+	public function say()	{
+		return 'humanity man: hello';
+	}
+}
